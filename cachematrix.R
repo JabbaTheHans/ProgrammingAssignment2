33d0a3b13ee3dcf215609cfee57863b7c04e51dd
JabbaTheHans <hfrbosshard@gmail.com>
2020-11-09 16:27:06 +0300

    Submission for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Here are 2 functions that do the following:
 
-## Write a short comment describing this function
+# 1) caches the entered matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrix_cache<<-solve(x)
+  matrix_entered<<-x
 }
 
-
-## Write a short comment describing this function
+# 2) Gets the inverse of a matrix entered, but will simply return the cached
+# matrix from function in #1 if the result is the same as that of the cache 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  solved_matrix<-solve(x)
+    if(identical(solved_matrix,matrix_cache)){solved_matrix<-matrix_cache}
+    print(solved_matrix)
 }
